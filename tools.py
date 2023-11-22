db50b94ed582309577eb11e2912737a0ebd66e54
Yaroslav Baienko <zerhug@gmail.com>
2023-11-23 00:17:58 +0200

    Initial commit

--- tools.py
+++ tools.py
@@ -1,12 +1,4 @@
 import ipaddress
-<<<<<<< HEAD
-
-ipv4 = ipaddress.ip_address('10.0.1.1')
-subnet1 = ipaddress.ip_network('80.0.1.0/28')
-netmask = subnet1.with_netmask
-subsub = list(subnet1.subnets(prefixlen_diff=2))
-print(subsub)
-=======
 import math
 
 
